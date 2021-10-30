dd3749fbe66bb76d523eea6d5799436c5e6cd12c
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2021-10-30 23:40:01 +0200

    GetItem by Id

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -1,25 +1,39 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using WEBAPI.Entities;
 using WEBAPI.Repositories;
 
-namespace WEBAPI.Controllers 
+namespace WEBAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class ItemsController : ControllerBase 
+    [Route("items")]
+    public class ItemsController : ControllerBase
     {
-        private readonly InMemItemsRepository repository; 
+        private readonly InMemItemsRepository repository;
 
-        public ItemsController()
+        public ItemsController(InMemItemsRepository repo)
         {
-            repository = new InMemItemsRepository();
+            this.repository = repo;
         }
 
         [HttpGet]
-        public IEnumerable<Item> GetItems() 
+        public IEnumerable<Item> GetItems()
         {
             return repository.GetItems();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
         }
     }
 }
